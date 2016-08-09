38d141e181d0d0e25681c94e485bd42946e1c3ab
Ian Boucher <i.a.boucher@gmail.com>
2016-08-09 16:21:46 +0100

    Comlete cp8 tutorial

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,32 +1,32 @@
 (function ()
 {
-    function SongPlayer()
+    function SongPlayer(Fixtures) // Inject Fixtures service to enable access to album info
     {
-        var SongPlayer        = {};
-            
-        /*
-        * @desc Stores the currently playing song
-        * @type {Object}
-        */
-        var currentSong       = null;
+        var SongPlayer = {};
         
         /*
         * @desc Buzz object audio file
         * @type {Object}
         */
         var currentBuzzObject = null;
-        
+                
+        /*
+        * @desc Stores current album information
+        * @type {Object}
+        */
+        var currentAlbum = Fixtures.getAlbum();
+
         /*
         * @function setSong
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
         * @param {Object} song
-        */
+        */        
         function setSong(song)
         {
             if (currentBuzzObject)
             {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
 
             currentBuzzObject = new buzz.sound(song.audioUrl, 
@@ -35,7 +35,7 @@
                 preload: true
             });
 
-            currentSong = song;
+            SongPlayer.currentSong = song;
         }
         
         /*
@@ -48,7 +48,24 @@
             currentBuzzObject.play();
             song.playing = true;
         }
-              
+               
+        /*
+        * @function getSongIndex
+        * @desc Finds the index of 'song' within the album song list
+        * @param {Object} song
+        * @returns {Number}
+        */
+        function getSongIndex(song)
+        {
+            return currentAlbum.songs.indexOf(song);
+        }
+                    
+        /*
+        * @desc Stores the currently playing song
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;
+        
         /*
         * @function play
         * @desc Plays the selected new or paused song
@@ -56,12 +73,15 @@
         */
         SongPlayer.play = function(song)
         {
-            if (currentSong !== song)
+            // enable PlayerBar to use this method without access to 'song' object
+            song = song || SongPlayer.currentSong; 
+            
+            if (SongPlayer.currentSong !== song)
             {
                 setSong(song);
                 playSong(song);
             }
-            else if (currentSong === song)
+            else if (SongPlayer.currentSong === song)
             {
                 if (currentBuzzObject.isPaused())
                 {
@@ -77,8 +97,35 @@
         */
         SongPlayer.pause = function(song)
         {
+            // enable PlayerBar to use this method without access to 'song' object
+            song = song || SongPlayer.currentSong;
+            
             currentBuzzObject.pause();
             song.playing = false;
+        };
+        
+        /*
+        * @function previous
+        * @desc Sets the current song to the previous song from the album list
+        * @param {Object} song
+        */
+        SongPlayer.previous = function(song)
+        {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex -= 1;
+            
+            if (currentSongIndex < 0)
+            {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            }
+            else
+            {
+                var song = currentAlbum.songs[currentSongIndex];
+                
+                setSong(song);
+                playSong(song);
+            }
         };
         
         return SongPlayer;
