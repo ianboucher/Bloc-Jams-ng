f014a4c5fe5258064bf310ad752582b0eae53df2
Ian Boucher <i.a.boucher@gmail.com>
2016-08-11 15:34:05 +0100

    Complete cp11 tutorial

---
+++ dist/scripts/filters/timecode.js
@@ -0,0 +1,35 @@
+(function() 
+{
+    function timecode () 
+    {
+        return function (timeInSeconds) 
+        {
+            timeInSeconds = Number.parseFloat(timeInSeconds)
+
+            var formattedTime    = null;
+            var wholeSeconds     = Math.floor(timeInSeconds);
+            var minutes          = Math.floor(wholeSeconds / 60);
+            var remainingSeconds = (wholeSeconds% 60);
+            
+            if (Number.isNaN(timeInSeconds))
+            {
+                formattedTime = "-:--";
+            }
+            else 
+            {
+                if (remainingSeconds < 10) 
+                { 
+                    remainingSeconds = "0" + remainingSeconds
+                };
+
+                formattedTime = minutes + ":" + remainingSeconds;
+            }
+    
+            return formattedTime;
+        };
+    }
+
+    angular
+     .module("blocJams")
+     .filter("timecode", timecode);
+})();
