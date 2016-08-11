52c429b4ba53a959dd7ab8bbfe0a6b96716ce838
Ian Boucher <i.a.boucher@gmail.com>
2016-08-11 15:59:24 +0100

    Complete cp11 assignment

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -4,28 +4,7 @@
     {
         return function (timeInSeconds) 
         {
-            timeInSeconds = Number.parseFloat(timeInSeconds)
-
-            var formattedTime    = null;
-            var wholeSeconds     = Math.floor(timeInSeconds);
-            var minutes          = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = (wholeSeconds% 60);
-            
-            if (Number.isNaN(timeInSeconds))
-            {
-                formattedTime = "-:--";
-            }
-            else 
-            {
-                if (remainingSeconds < 10) 
-                { 
-                    remainingSeconds = "0" + remainingSeconds
-                };
-
-                formattedTime = minutes + ":" + remainingSeconds;
-            }
-    
-            return formattedTime;
+            return buzz.toTimer(timeInSeconds)
         };
     }
 
